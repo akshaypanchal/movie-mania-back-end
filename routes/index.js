527abafffdc5735c49759471d9a3977e24443137
unknown <akshaypanchal67@gmail.com>
2021-01-12 19:43:06 -0500

    added all the node packages and store the signup data coming fron front-end to mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+
+var url = "mongodb://localhost:27017/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("success");
+});
+
+
+router.post('/signup', function(req, res, next) {
+  console.log(req.body);
+
+  MongoClient.connect(url, { useUnifiedTopology: true },
+    function (err, db) {
+      if (err) {
+        throw err;
+      }
+      else {
+        var dbo = db.db("Movie-Mania-DB");
+        dbo.collection('users').insertOne(req.body);
+      }
+      res.status(201).send("success");
+
+
+    });
+
 });
 
 module.exports = router;

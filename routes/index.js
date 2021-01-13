718de3a3e1d96657499b64bfd9f36002fcdb17d1
unknown <akshaypanchal67@gmail.com>
2021-01-12 21:42:25 -0500

    implement the login functionality with cookie settings for front end

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-
+var jwt = require('jsonwebtoken');
 
 var url = "mongodb://localhost:27017/";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send("success");
 });
 
 
-router.post('/signup', function(req, res, next) {
+router.post('/signup', function (req, res, next) {
   console.log(req.body);
 
   MongoClient.connect(url, { useUnifiedTopology: true },
@@ -26,6 +26,63 @@ router.post('/signup', function(req, res, next) {
       res.status(201).send("success");
 
 
+    });
+
+});
+
+
+router.post("/login", function (req, res, next) {
+
+  let email = req.body.email;
+  let password = req.body.password;
+
+
+
+  MongoClient.connect(url, { useUnifiedTopology: true },
+    function (err, db) {
+      if (err) {
+        throw err;
+      }
+      else {
+        var dbo = db.db("Movie-Mania-DB");
+        dbo.collection('users').find({ $and: [{ "email": email }, { "password": password }] }).toArray(function (err, result) {
+          if (err) {
+            throw err;
+          }
+          else {
+            // res.status(200).json({ data: result }););
+            if (result.length > 0) {
+              console.log("function called");
+
+              const token = jwt.sign(
+                // payload
+                { user: email },
+                // sercret key
+                "dreams",
+                // header 
+                {
+                  algorithm: "HS256",
+                  expiresIn: 3600   //seconds
+                }
+              )
+
+              // set it the response's cookie
+              res.json({ 'AuthToken' : token, 'maxAge' :  3600  });
+              // res.redirect('/shopping');
+              res.send("success");
+
+            }
+
+            else {
+              res.send("Invalid login details");
+            }
+
+            // res.status(200);
+            res.end();
+            db.close();
+          }
+        });
+      }
     });
 
 });
